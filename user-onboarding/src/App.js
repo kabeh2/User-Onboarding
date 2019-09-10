be8c5f032156132d96b0633fd1effae2ed7cc2da
kabeh2 <mattcnk@hotmail.com>
2019-09-10 10:07:00 -0400

    Initial commit with some form structure and validation using formik and yup

---
+++ user-onboarding/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import { ValidationSchemaExample } from "./components/forms/form";
+
+function App() {
+  return (
+    <div className="App">
+      <ValidationSchemaExample />
+    </div>
+  );
+}
+
+export default App;
